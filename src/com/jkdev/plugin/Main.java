daab0673852d8ef9f53979df837717b72c8272be
JkDevArg <jkdevarg@gmail.com>
2021-12-16 14:57:24 -0500

    Rev: 0.3

--- src/com/jkdev/plugin/Main.java
+++ src/com/jkdev/plugin/Main.java
@@ -1,17 +1,24 @@
 package com.jkdev.plugin;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin{
+	PluginDescriptionFile pluginyml = getDescription();
+	public String version = pluginyml.getVersion();
+	public String nombre = ChatColor.GREEN+"["+ChatColor.AQUA+pluginyml.getName()+ChatColor.GREEN+"]";
+	
 	
 	@Override
 	public void onEnable() {
-		System.out.println("Plugin Iniciado correctamente");
+		Bukkit.getConsoleSender().sendMessage(nombre+ChatColor.WHITE+"  Ha sido activado (version: "+ChatColor.RED+version+ChatColor.WHITE+")");
 		getServer().getPluginManager().registerEvents(new BlockBreak(), this);
 		
 	}
 	
 	@Override
 	public void onDisable() {
-		System.out.println("Plugin Desactivado Bye bye!");
+		Bukkit.getConsoleSender().sendMessage(nombre+ChatColor.RED+" Ha sido desactivado");
 	}
 }
